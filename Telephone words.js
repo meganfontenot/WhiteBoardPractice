51e44c5ea9a51168ec35fe87a751532400d82a63
Megan Fontenot <meganfontenot517@yahoo.com>
2020-03-21 13:40:49 -0500

    working on solution have letters mapped out

--- Telephone words.js
+++ Telephone words.js
@@ -47,3 +47,18 @@ const telephoneWords = function(str) {
 
 console.log(telephoneWords('2745'));
 telephoneWords('2745');
+
+//  SOLUTION  //
+
+const phoneDigitsToLetters = {
+  0: '0',
+  1: '1',
+  2: 'ABC',
+  3: 'DEF',
+  4: 'GHI',
+  5: 'JKL',
+  6: 'MNO',
+  7: 'PQRS',
+  8: 'TUV',
+  9: 'WXYZ',
+};
