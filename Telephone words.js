60907f071060e4a91af67bed68cdcc9cbeab71c5
Megan Fontenot <meganfontenot517@yahoo.com>
2020-03-21 13:39:47 -0500

    Functions worked out

--- Telephone words.js
+++ Telephone words.js
@@ -8,24 +8,42 @@ const phoneDigitsToLetters = {
   6: 'MNO',
   7: 'PQRS',
   8: 'TUV',
-  9: 'WXYZ'
+  9: 'WXYZ',
 };
 
-const telephoneWords = function (str) {
+const telephoneWords = function(str) {
   const target = [];
   let i;
   let j;
   const digitArr = str.split('');
-    if (str) {
-      for (i = 0; i < digitArr.length; i++) {
+  if (str) {
+    for (i = 0; i < digitArr.length; i++) {
       target.push(phoneDigitsToLetters[str[i]]);
-      }
     }
+  }
 
   function combinations(arr) {
     const newArr = arr.slice(0);
     if (newArr.length === 0) {
       return [];
     }
-    
-    
+
+    let res = [];
+    const top = newArr.shift();
+    const ret = combinations(newArr);
+    if (ret.length === 0) {
+      res = top;
+    } else {
+      for (i = 0; i < top.length; i++) {
+        for (j = 0; j < ret.length; j++) {
+          res.push(top[i] + ret[j]);
+        }
+      }
+    }
+    return res;
+  }
+  return combinations(target);
+};
+
+console.log(telephoneWords('2745'));
+telephoneWords('2745');
